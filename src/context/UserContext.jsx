70838baea879c11a0d14eb657d784ede44224e06
robert-loera <robert.loera.3rd@gmail.com>
2022-06-22 15:00:53 -0500

    changed noti url

--- src/context/UserContext.jsx
+++ src/context/UserContext.jsx
@@ -19,7 +19,7 @@ export const UserProvider = (props) => {
       }
     }
     // make the request to the API
-    const response = await fetch("http://web3socialz.herokuapp.com/usersme", requestOptions)
+    const response = await fetch("https://web3socialz.herokuapp.com/usersme", requestOptions)
 
     // if the response is not ok set the token to null
     if (!response.ok) {
